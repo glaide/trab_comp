7ecfdc92c511d42b6010b8e1e1412f5bfaaf29df
Glaide de Lara Oliveira <glaide@craftpro.com.br>
2023-11-21 11:39:56 -0300

    teste

--- tabelaSimbolos.c
+++ tabelaSimbolos.c
@@ -157,7 +157,7 @@ void atualizaNumeroVariaveis(TypeTabelaSimbolosPilha *p, int vars, int nivel_lex
         aux = aux->prox;
     }
     printf("Valor de aux->categoria%d\n", aux->categoria);
-    while (aux->categoria != procedimento && aux->categoria != funcao)
+    while (aux->categoria != procedimento && aux->categoria != funcao && aux->prox->prox != NULL)
     {
         aux = aux->prox;
     }
